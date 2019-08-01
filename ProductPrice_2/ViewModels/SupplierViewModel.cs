9652c888c397ea15d958a7c6c4618145664fd608
Masumi P <44107410+lovelygsd2018@users.noreply.github.com>
2019-08-01 08:26:25 -0500

    Add files via upload

---
+++ ProductPrice_2/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductPrice_2.ViewModels
+{
+    public class SupplierViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
