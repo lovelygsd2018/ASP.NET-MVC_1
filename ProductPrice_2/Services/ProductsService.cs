c3700695c6f3c06cc6a63f9ae2e13da7e4ca7b3f
Masumi P <44107410+lovelygsd2018@users.noreply.github.com>
2019-08-01 08:22:13 -0500

    Add files via upload

---
+++ ProductPrice_2/Services/ProductsService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductPrice_2.Interfaces;
+using ProductPrice_2.Models;
+using ProductPrice_2.Repositories;
+using ProductPrice_2.ViewModels;
+using Microsoft.AspNetCore.Mvc; //IActionResult
+
+namespace ProductPrice_2.Services
+{
+    public class ProductsService : IProductService
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public ProductsService(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        public async   Task<IActionResult> GetAllProductsAsync()
+        {
+            try{
+                IEnumerable<Product> products = await _productsRepository.GetAllProductsAsync();
+                if(products != null){
+                    return new OkObjectResult(products.Select(p => new ProductViewModel{
+                        Id = p.ProductId, Name = p.Name })
+                    );
+                }else{
+                    return new NotFoundResult();
+                }
+            }catch {
+                return new ConflictResult();
+            }
+        }
+
+        public async   Task<IActionResult> GetProductbyIdAsync(int productId)
+        {
+            try{
+                Product prod = await _productsRepository.GetProductbyIdAsync(productId);
+                if(prod != null){
+                    return new OkObjectResult(new ProductViewModel()
+                    {
+                        Id = prod.ProductId,
+                        Name = prod.Name
+                    });;
+                }else{
+                    return new NotFoundResult();
+                }
+            }catch{
+                return new ConflictResult();
+            }           
+        }
+    }
+}
---
+++ ProductPrice_2/Services/SuppliersService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductPrice_2.Interfaces;
+using ProductPrice_2.Models;
+using ProductPrice_2.Repositories;
+using ProductPrice_2.ViewModels;
+using Microsoft.AspNetCore.Mvc; //IActionResult
+
+namespace ProductPrice_2.Services
+{
+    public class SuppliersService : ISuppliersService
+    {
+        private readonly ISuppliersRepository _repo;
+        public SuppliersService(ISuppliersRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<IActionResult> GetAllSuppliersAsync()
+        {
+            try{
+                IEnumerable<Supplier> suppList = await _repo.GetAllSuppliersAsync();
+                if(suppList != null){
+                    //return new OkObjectResult(suppList.Select(s => new SupplierViewModel
+                    //{ Id = s.SupplierId, Name = s.Name })
+                    //);
+                    var list = suppList.Select(s => new SupplierViewModel
+                    {
+                        Id = s.SupplierId,
+                        Name = s.Name
+                    });
+                    return new OkObjectResult(list);
+                }else{
+                    return new  NotFoundResult();
+                }
+            }catch{
+                return  new ConflictResult();
+            }
+        }
+
+        public async Task<IActionResult> GetSupplierbyIdAsync(int supplierId){
+            try{
+                var supp = await _repo.GetSupplierbyIdAsync(supplierId);
+                if (supp == null){
+                    return new NotFoundResult();
+                }else{
+                    return new OkObjectResult(supp);
+                }
+            }catch{
+                return new ConflictResult();
+            }
+        }
+    }
+}
