7be7672de7bc18bff13e10d7a34e262929676c6b
Masumi P <44107410+lovelygsd2018@users.noreply.github.com>
2019-08-01 08:18:39 -0500

    Add files via upload

---
+++ ProductPrice_2/Repositories/ProductsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductPrice_2.Interfaces;
+using ProductPrice_2.Contexts;
+using ProductPrice_2.Models;
+using Microsoft.EntityFrameworkCore;    //.ToListAsync();
+
+
+namespace ProductPrice_2.Repositories
+{
+    public class ProductsRepository : IProductsRepository
+    {
+        private readonly    DefaultContext _context;
+        public ProductsRepository(DefaultContext    context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        public async Task<Product> GetProductbyIdAsync(int productId)
+        {
+            return await _context.Products.Where(p => p.ProductId == productId).FirstOrDefaultAsync();
+        }
+
+    }
+}
---
+++ ProductPrice_2/Repositories/SuppliersRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductPrice_2.Interfaces;
+using ProductPrice_2.Models;
+using ProductPrice_2.Contexts;
+using Microsoft.EntityFrameworkCore;    //.ToListAsync()
+
+namespace ProductPrice_2.Repositories
+{
+    public class SuppliersRepository : ISuppliersRepository
+    {
+        private readonly DefaultContext _context;
+        public SuppliersRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async    Task<IEnumerable<Supplier>> GetAllSuppliersAsync()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+        public async Task<Supplier> GetSupplierbyIdAsync(int supplierId)
+        {
+            return await _context.Suppliers.Where(sup => sup.SupplierId == supplierId).FirstOrDefaultAsync();
+        }
+    }
+}
