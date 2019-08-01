aed84d6f99f3f16d052d01b9b9e3ecd1e5b6ee6d
Masumi P <44107410+lovelygsd2018@users.noreply.github.com>
2019-08-01 07:51:21 -0500

    Add files via upload

---
+++ ProductPrice_2/Controllers/SuppliersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ProductPrice_2.Interfaces;
+//using Microsoft.AspNetCore;
+
+namespace ProductPrice_2.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/SuppList")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ISuppliersService _service;
+        public SuppliersController(ISuppliersService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async    Task<IActionResult> GetAllSuppliers()
+        {
+            return await _service.GetAllSuppliersAsync();
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult>    GetSupplierById(int id)
+        {
+            return await _service.GetSupplierbyIdAsync(id);
+        }
+    }
+}
